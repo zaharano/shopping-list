4d9f28896b8725cd04ccade885e4c8c31a1d3e25
Zach <zraisnorman@gmail.com>
2024-05-08 10:09:28 -0400

    adding comments

--- project2.py
+++ project2.py
@@ -8,12 +8,81 @@ from pint import Unit
 
 DEBUG_MODE = True
 
+STOP_INPUTS = ['', None, ' ', 'stop', 'exit', 'quit']
+
+# feedback colors
+ERR = 'red'
+WARN = 'yellow'
+INFO = 'cyan'
+GOOD = 'green'
+
 # adding items leads to .0 stuff
 # plurality upon adding
+# errors / handling in add_item, existing_item, elsewhere
+# move categorizer into main file for submit
+# write some tests
+
+def main():
+  OPTIONS = [
+    "Add ingredients from recipe to shopping list (by URL, works with most recipe pages)", 
+    "Add items to shopping list", 
+    "Remove items from the shopping list",
+    "View current shopping list", 
+    "Export list and quit",
+  ]
+  DEBUG_OPTIONS = [
+    "Print an item's parsed data (for debugging)"
+  ]
+
+  # DEBUG MODE set at top of this file
+  if DEBUG_MODE:
+    OPTIONS += DEBUG_OPTIONS
+    
+  terminal_menu = TerminalMenu(OPTIONS)
+
+  shopping_list = ShoppingList()
+
+  while(True):
+    terminal_menu = TerminalMenu(OPTIONS, status_bar="Items: " + str(shopping_list.length()) + " | Recipes: " + str(len(shopping_list.recipes)))
+    menu_entry_index = terminal_menu.show()
+
+    if menu_entry_index == 0:
+      add_recipe_by_url(shopping_list)
+    elif menu_entry_index == 1:
+      add_items_to_list(shopping_list)
+    elif menu_entry_index == 2:
+      remove_items_from_list(shopping_list)
+    elif menu_entry_index == 3:
+      view_list(shopping_list)
+    elif menu_entry_index == 4:
+      export_list(shopping_list)
+      return 0
+
+    # DEBUG OPTIONS
+    elif menu_entry_index == 5:
+      select = item_select(shopping_list.items)
+      if select == -1:
+        continue
+      print(shopping_list._items[select].parsed)
+      print(shopping_list._items[select])
 
 class ShoppingList:
+  '''
+  A class to represent a shopping list.
+
+  The shopping list is a collection of ingredients and recipes. It provides methods to add and remove items, and to categorize the items.
+
+  Parameters
+  ----------
+  items : list, optional
+    A list of strings to initialize the shopping list with. Default is an empty list.
+  '''
   def __init__(self, items = []):
-    self._items = items
+    if type(items) != list:
+      raise TypeError("Items must be a list.")
+    if not all(isinstance(x, str) for x in items):
+      raise TypeError("Items must be a list of strings.")
+    self._items = list(map(lambda x: Ingredient(x), items))
     self._recipes = []
     
   def __str__(self):
@@ -51,46 +120,102 @@ class ShoppingList:
   def length(self):
     return len(self._items)
 
+  # add an item to the shopping list
+  # returns 0 if successful, 1 if existing item was added to, 
+  # 2 if existing item was not added to, 3 if item was not parsed
   def add_item(self, item, coeff = 1):
+    # change this I think - don't want it to choke on empty strings
     if not item:
       raise TypeError("No item to add.")
     try:
       new_item = Ingredient(item.strip())
     except Exception as e:
-      raise ParseException("Couldn't parse that item. Please try again.")
-    # Apply the coefficient if there is one
+      raise ParseException(f"Couldn't parse that item - {item}. Please try again.")
     if coeff != 1:
       try:
         new_item = new_item * coeff
       except Exception as e:
         cprint(e, 'red')
-    if self.existing_item(new_item):
-      return
+    existing_item = self.existing_item(new_item)
+    if existing_item:
+      return 1
     self._items.append(new_item)
+    return 0
  
   # if ingredient with same name exists, attempt to add them
+  # return 0 if there is no existing item
+  # return 1 if existing item was added to, 2 if existing item was not added to
   # longterm TODO: account for different ways of writing the same ingredient
   def existing_item(self, new_item):
+    '''
+    Given a parsed Ingredient object, check if an item with the same name exists in the list of items. If it does, attempt to add the new item to the existing item. Return a result code.
+
+    Parameters:
+    ----------
+    new_item : Ingredient
+      the new item to check against the existing items
+
+    Returns:
+    -------
+    bool
+      True if the item was added to an existing item, False if it was not
+    '''
     for i in self._items:
       if singularize(i.name) == singularize(new_item.name):
-        # if they have pint units, try to add them
-        # TODO: fix that this is only one confirmed pint unit
-        try:
-          i = i + new_item
-          return True
-        except ValueError or TypeError as e:
-          print("fuck" + e)
-          return True
+        i = i + new_item
+        return True
     return False
     
   def remove_item(self, index):
-    del self._items[int(index)]
+    '''
+    Given an index, remove the item at that index from the list of items.
+
+    Parameters:
+    ----------
+    index : int
+      The index of the item to remove from the _items list.
+
+    Returns:
+    -------
+    None
+    '''
+    if index >= 0 and index < len(self._items):
+      del self._items[int(index)]
+    else:
+      raise ValueError("Invalid delete index.")
 
   def add_recipe(self, recipe):
-    # TODO: don't append recipe if it's already there
-    self._recipes.append(recipe)
+    '''
+    Given a Recipe object, append it to the list of recipes, and add its ingredients to the list of items.
+
+    If a recipe with the same title already exists, don't append it to the list of recipes (but still add its ingredients to the shopping list).
+
+    Parameters:
+    ----------
+    recipe : Recipe
+      The recipe to add to the shopping list.
+
+    Returns:
+    -------
+    None
+    '''
+    exists = False
+    issues = False
+    for existing_recipe in self._recipes:
+      if existing_recipe.title == recipe.title:
+        exists = True
+    if not exists:
+      self._recipes.append(recipe)
     for ingredient in recipe.ingredients:
-      self.add_item(ingredient, recipe.coeff)
+      if ingredient:
+        try:
+          self.add_item(ingredient, recipe.coeff)
+        except Exception as e:
+          cprint(e, 'red')
+          issues = True
+    if issues:
+      return 1
+    return 0
 
 class Recipe:
   def __init__(self, title, ingredients, yields, url, data = None, coeff = 1):
@@ -138,6 +263,11 @@ class Ingredient:
   A class to represent an ingredient.
 
   Given a string (hopefully an ingredient string), it will parse the string using the ingredient_parser library, provide some simple ways to interact with that parsed data. It will also categorize the ingredient using the ingredient_categorizer.
+
+  Parameters
+  ----------
+  text : str
+    The ingredient string to parse and categorize.
   """
   def __init__(self, text):
     self._sentence = text
@@ -187,6 +317,8 @@ class Ingredient:
         a = self.unit * float(self.quantity)
         b = other.unit * float(other.quantity)
         self.quantity = str((a + b).magnitude)
+      else:
+        raise ValueError("Can't add ingredients with incompatible units.")
     elif type(self.unit) == str and type(other.unit) == str:
       if self.unit == other.unit:
         self.quantity = str(float(self.quantity) + float(other.quantity))
@@ -264,8 +396,12 @@ def item_select(items):
     return -1
   return menu_entry_index
 
+# Add ingredients from recipe to shopping list
+# requests URL, scrapes recipe, adds ingredients to shopping list
 def add_recipe_by_url(shopping_list):
   url = input("Enter the URL of the recipe: ")
+  if item in STOP_INPUTS:
+    return
   try: 
     scraper = scrape_me(url)
   except: 
@@ -294,17 +430,20 @@ def add_recipe_by_url(shopping_list):
         except:
           cprint("Not a valid coefficient. Enter a number more than zero and less than 100.", 'red')
   new_recipe = Recipe(scraper.title(), scraper.ingredients(), scraper.yields(), url, scraper, coeff)
-  shopping_list.add_recipe(new_recipe)
-  cprint(f"Added items from recipe. The shopping list now has {shopping_list.length()} items.", 'green')
+  result = shopping_list.add_recipe(new_recipe)
+  if result == 1:
+    cprint(f"There were issues adding some ingredients from the recipe. The shopping list now has {shopping_list.length()} items", 'yellow')
+  else:
+    cprint(f"Added items from recipe. The shopping list now has {shopping_list.length()} items.", 'green')
 
 def add_items_to_list(shopping_list):
   while(True):
     item = input("Enter an item to add (enter nothing to stop adding items): ")
-    if item == '':
+    if item in STOP_INPUTS:
       break
     try:
-      shopping_list.add_item(item)
-      cprint(f"Added item. The shopping list now has {shopping_list.length()} items.", 'green')
+      if shopping_list.add_item(item) == 0:
+        cprint(f"Added item. The shopping list now has {shopping_list.length()} items.", 'green')
     except TypeError as e:
       cprint(e, 'red')
 
@@ -343,52 +482,6 @@ def export_list(shopping_list):
     for recipe in shopping_list.recipes:
       file.write(str(recipe) + '\n')
     cprint(f"Exported {shopping_list.length()} item long list to {filename}.", 'green')
-
-def main():
-  OPTIONS = [
-    "Add ingredients from recipe to shopping list (by URL, works with most recipe pages)", 
-    "Add items to shopping list", 
-    "Remove items from the shopping list",
-    "View current shopping list", 
-    "Export list and quit",
-  ]
-  DEBUG_OPTIONS = [
-    "Print an item's parsed data (for debugging)"
-  ]
-
-  # DEBUG MODE set at top of this file
-  if DEBUG_MODE:
-    OPTIONS += DEBUG_OPTIONS
-    
-  terminal_menu = TerminalMenu(OPTIONS)
-
-  shopping_list = ShoppingList()
-
-  while(True):
-    terminal_menu = TerminalMenu(OPTIONS, status_bar="Items: " + str(shopping_list.length()) + " | Recipes: " + str(len(shopping_list.recipes)))
-    menu_entry_index = terminal_menu.show()
-
-    # Add ingredients from recipe to shopping list
-    # requests URL, scrapes recipe, adds ingredients to shopping list
-    if menu_entry_index == 0:
-      add_recipe_by_url(shopping_list)
-    elif menu_entry_index == 1:
-      add_items_to_list(shopping_list)
-    elif menu_entry_index == 2:
-      remove_items_from_list(shopping_list)
-    elif menu_entry_index == 3:
-      view_list(shopping_list)
-    elif menu_entry_index == 4:
-      export_list(shopping_list)
-      return 0
-
-    # DEBUG OPTIONS
-    elif menu_entry_index == 5:
-      select = item_select(shopping_list.items)
-      if select == -1:
-        continue
-      print(shopping_list._items[select].parsed)
-      print(shopping_list._items[select])
 
 if __name__ == "__main__":
   main()
